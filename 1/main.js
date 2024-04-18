44877ec89d4482856f67752eced9cdf318b84959
BSbenjaminsoto <benjaminsoto661@hotmail.com>
2024-04-17 22:11:12 -0400

    Fusionando componentes de html

---
+++ 1/main.js
@@ -0,0 +1,112 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
+const  cardsContainer = document.querySelector('.cards-container')
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu)
+menuShoppingCart.addEventListener('click',toggleShoppingCart)
+
+function toggleDesktopMenu(){
+    const isAsideClosed = productDetail.classList.contains('inacvite');
+
+    if(!isAsideClosed){
+        productDetail.classList.add('inactive');
+    }
+    
+    desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = productDetail.classList.contains('inacvite');
+
+    if(!isAsideClosed){
+        productDetail.classList.add('inactive');
+        
+    }
+    
+    mobileMenu.classList.toggle("inactive")
+
+
+}
+
+function toggleShoppingCart(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inacvite');
+    const isMenuDesktopOpen = desktopMenu.classList.contains('inactive')
+    //productDetail.classList.toggle("inactive")
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if(!isMenuDesktopOpen){
+        desktopMenu.classList.toggle("inactive")
+    }
+
+
+    productDetail.classList.toggle('inactive')
+    
+    
+}
+
+
+const productList = [];
+productList.push({
+    name:'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+productList.push({
+    name:'Pantalla',
+    price: 300,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+productList.push({
+    name:'Compu',
+    price: 300,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+    
+        const productImage = document.createElement('img')
+        productImage.setAttribute('src' , product.image)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+        
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name
+    
+        productInfoDiv.appendChild(productName)
+        productInfoDiv.appendChild(productPrice)
+    
+        const productInfoFigure = document.createElement('figure');
+    
+        const productCartIcon = document.createElement('img');
+        productCartIcon.setAttribute('src', './Platzi_YardSale_Icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productCartIcon);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImage);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
+};
+
+renderProducts(productList);
